703fbd5c9cc2df8c9fd2241d5eec6e4451488f0d
b <comonoid@protonmail.com>
2024-06-10 01:50:16 +0000

    tdl-install-types: better support for prebuilt-tdlib
    Can now load tdlib commit hash and tdlib version from
    prebuilt-tdlib/package.json, prebuilt-tdlib/prebuilds/tdlib.json (as a
    fallback for older prebuilt-tdlib versions), or falling back to the old
    behavior of extracting commit hash from
    require('prebuilt-tdlib').getTdjson().
    
    Added a special <target> `prebuilt-tdlib` instead of using the
    `--prebuilt-tdlib` option. `--prebuilt-tdlib` is deprecated.
    
    Some misc. improvements. tdl-install-types now fails if called with too
    many arguments.

--- scripts/update-types.sh
+++ scripts/update-types.sh
@@ -2,4 +2,4 @@
 
 # Should be executed with project root as the cwd
 
-npx tdl-install-types -o typings/tdlib-types.d.ts --flow --prebuilt-tdlib
+npx tdl-install-types -o typings/tdlib-types.d.ts --flow prebuilt-tdlib
